e3a30d0e08e96e7923784c8391c032652a84ade0
ketutk <ketutkrisnakertajaya@gmail.com>
2023-07-14 22:24:41 +0700

    Loader component centered

--- frontend/src/components/SubComponents/loader.js
+++ frontend/src/components/SubComponents/loader.js
@@ -1,7 +1,7 @@
 export default function LoadingSpinner() {
   return (
     <div className="d-flex justify-content-center position-relative" style={{ height: "80%" }}>
-      <div className="spinner-border text-info position-absolute" style={{ width: "10em", height: "10em", top: "40%", left: "40%" }} role="status">
+      <div className="spinner-border text-info position-absolute" style={{ width: "10em", height: "10em", top: "50%", left: "50%", marginTop: "-5em", marginLeft: "-5em" }} role="status">
         <span className="visually-hidden">Loading...</span>
       </div>
     </div>
